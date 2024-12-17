b0447465453ab365b4c21410f00f12b26fcb03d5
Francisco Ferreira <francisco.ferreirawde@gmail.com>
2024-12-17 20:48:01 -0300

    fixed game rank

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,5 +1,5 @@
 function createBodyTableRank() {
-  const storageRank = jSON.parse(localStorage.getItem("@spaceshipGame:rank"));
+  const storageRank = JSON.parse(localStorage.getItem("@spaceshipGame:rank"));
 
   let rankSorted;
   if (storageRank) {
@@ -7,25 +7,26 @@ function createBodyTableRank() {
       .sort((a, b) => b.score - a.score)
       .filter((_v, index) => index < 10);
   }
+
   const bodyTableRank = document.querySelector(".tableRank tbody");
   bodyTableRank.innerHTML = "";
+
   for (let i = 0; i < 10; i++) {
     bodyTableRank.innerHTML += `
-    <tr>
-        <td>${i + 1}</td>
-        <td>${rankSorted ? rankSorted[i]?.name || "" : ""}</td>
-        <td>${rankSorted ? rankSorted[i]?.score || "" : ""}</td>
-     </tr>
+      <tr>
+          <td>${i + 1}</td>
+          <td>${rankSorted ? rankSorted[i]?.name || "" : ""}</td>
+          <td>${rankSorted ? rankSorted[i]?.score || "" : ""}</td>
+      </tr>
      `;
   }
 }
 
 function backPage() {
-    window.history.back();
-
+  window.history.back();
 }
 
 const backButton = document.querySelector(".backButton");
-backButton.addEventLister("click", backPage);
+backButton.addEventListener("click", backPage);
 
 createBodyTableRank();
